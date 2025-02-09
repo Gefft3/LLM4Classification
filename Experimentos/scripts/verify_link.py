5d38217baf1bd75f3526534d11d9deeb09179762
Geffte <gefft3@gmail.com>
2025-02-09 16:53:44 -0400

    refactor: enhance driver creation and error handling in URL expansion script

--- Experimentos/scripts/verify_link.py
+++ Experimentos/scripts/verify_link.py
@@ -4,27 +4,29 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from tqdm import tqdm
+import time
 
 def create_driver():
-    options = Options()
-    options.add_argument('--headless') 
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.binary_location = '/usr/bin/google-chrome-stable' 
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    
+    prefs = {"profile.managed_default_content_settings.javascript": 2}
+    chrome_options.add_experimental_option("prefs", prefs)
     
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
-
-    return driver
-
+    return webdriver.Chrome(service=service, options=chrome_options)
 
 def expand_short_url(driver, short_url, retries=3, wait_time=5):
     for attempt in range(retries):
         try:
+            time.sleep(1)
             driver.get(short_url)
             return driver.current_url
         except Exception as e:
             if attempt < retries - 1:
+                time.sleep(wait_time)  
                 continue
             else:
                 raise e
@@ -39,18 +41,21 @@ def main():
 
     driver = create_driver()
 
-    for i, row in tqdm(dataset_links.iterrows(), total=dataset_links.shape[0]):
-        short_url = row['links']
-        try:
-            expanded_url = expand_short_url(driver, short_url)
-            expanded_links.append({'index': i, 'short_url': short_url, 'expanded_url': expanded_url})
-            expanded_links_df = pd.DataFrame(expanded_links)
-            expanded_links_df.to_csv(output_csv_path, index=False)
-        except Exception as e:
-            with open(error_log_path, 'a') as error_file:
-                error_file.write(f"Error: {e}\n")
-                error_file.write(f"{i}\n")
-                error_file.write(f"------------------------\n")
-            
+    try:
+        for i, row in tqdm(dataset_links.iterrows(), total=dataset_links.shape[0]):
+            short_url = row['links']
+            try:
+                expanded_url = expand_short_url(driver, short_url)
+                expanded_links.append({'index': i, 'short_url': short_url, 'expanded_url': expanded_url})
+                expanded_links_df = pd.DataFrame(expanded_links)
+                expanded_links_df.to_csv(output_csv_path, index=False)
+            except Exception as e:
+                with open(error_log_path, 'a') as error_file:
+                    error_file.write(f"Error: {e}\n")
+                    error_file.write(f"{i}\n")
+                    error_file.write(f"------------------------\n")
+    finally:
+        driver.quit()  
+
 if __name__ == '__main__':
     main()
